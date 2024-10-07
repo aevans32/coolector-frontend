5f17ea4235328380e42a9f10d062d568d4962463
Andres Evans <andres_evans@outlook.com>
2024-10-06 21:26:45 -0500

    added the css as global in styles.css

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from "../dashboard/dashboard.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [DashboardComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
