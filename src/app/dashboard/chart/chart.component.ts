f556d0e202052fcdc1b501b4341ff544ad6b6b96
Andres Evans <andres_evans@outlook.com>
2024-10-06 23:40:24 -0500

    implemented the chart succesfully

--- src/app/dashboard/chart/chart.component.ts
+++ src/app/dashboard/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
@@ -9,5 +10,60 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrl: './chart.component.css'
 })
 export class ChartComponent {
+
+  // im setting type over in the template
+  // public lineChartType: ChartType = 'line';
+
+  // X axis labels
+  public lineChartLabels: string[] = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+
+  // data and some parameters
+  public lineChartDatasets: ChartDataset<'line'>[] = [
+    {
+      data: [
+        15339,
+        21345,
+        18483,
+        24003,
+        23489,
+        24092,
+        12034
+      ],
+      label: 'Weekly Data',
+      tension: 0,
+      backgroundColor: 'transparent',
+      borderColor: '#007bff',
+      borderWidth: 4,
+      pointBackgroundColor: '#007bff' 
+    }
+  ];
+
+  // options i guess...
+  public lineChartOptions: ChartOptions<'line'> = {
+    // responsive has to do with how big teh chart can get
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        padding: 3
+      }
+    },
+    scales: {
+      x: {},
+      y: {
+        beginAtZero: false
+      }
+    }
+  };
 
 }
