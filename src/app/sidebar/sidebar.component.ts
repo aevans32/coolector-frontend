9facd3431d11d6ec5e29bc30586beadcc7f0c16b
Andres Evans <andres_evans@outlook.com>
2024-11-14 22:22:19 -0500

    links to simulators are working well - now on to change the visuals and the value for the debts

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
